770d4f756769117801ff04518882ad9c2ba45003
Arun Natarajan <arunkmr492@gmail.com>
2018-08-09 15:48:26 +0100

    variable name changed

--- malicious_module.sh
+++ malicious_module.sh
@@ -10,11 +10,11 @@ echo "$(timestamp) - Malicious script launched"
 echo "$(timestamp) - Start Attack #1{attack_name_placeholder}"
 
 #Variable definition
-WP_APP_URL=$1
+WP_IP=$1
 
-echo "wp variable -- $WP_APP_URL -- $1"
+echo "wp variable -- $WP_IP -- $1"
 #Directory listing
-msfconsole -q -x "use auxiliary/scanner/http/dir_scanner; set RHOSTS $WP_APP_URL; run; exit;"
+msfconsole -q -x "use auxiliary/scanner/http/dir_scanner; set RHOSTS $WP_IP; run; exit;"
 
 #2 Arachni web testing
 # /root/Documents/arachni-security-test/bin/arachni "$WP_APP_URL"
