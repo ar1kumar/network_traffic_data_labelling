cf3cd88619aa86af31cdf1623d6d5013e87c387e
Arun Natarajan <arunkmr492@gmail.com>
2018-08-17 15:37:40 +0100

    silly typo fixed, changed comparison opeartor

--- handy.sh
+++ handy.sh
@@ -53,4 +53,4 @@ awk -v label="Benign" -v src_ip="10.0.2.4" -v dst_ip="10.0.2.6" '{ if (($2 != sr
 #If else statement - modify label if one or more conditions are satisfied.
 awk -v label_B="Benign" -v label_M="Malicious" -v src_ip="10.0.2.4" -v dst_ip="10.0.2.6" '{ if (($2 != src_ip && $2 != dst_ip) || ($4 != src_ip && $4 != dst_ip)) {$84=label_B}; else{$84=label_M}; print}' FS=, OFS=, 09-08-2018.experiment01.unlabelled.csv > 09-08-2018.experiment01.labelled.csv
 
-awk -v label_B="Benign" -v label_M="Malicious" -v malicious_ip="10.0.2.4" -v benign_ip="10.0.2.7" '{ if ($2 == benign_ip && $3 == benign_ip) {$84=label_B}; else{$84=label_M}; print}' FS=, OFS=, bitflow_data/14-08-18.experiment.captured.pcap_Flow.csv > bitflow_data/14-08-18.experiment.labelled.pcap_Flow.csv
+awk -v label_B="Benign" -v label_M="Malicious" -v malicious_ip="10.0.2.4" -v benign_ip="10.0.2.7" '{ if ($2 == benign_ip || $3 == benign_ip) {$84=label_B}; else{$84=label_M}; print}' FS=, OFS=, bitflow_data/14-08-18.experiment.captured.pcap_Flow.csv > bitflow_data/14-08-18.experiment.labelled.pcap_Flow.csv
